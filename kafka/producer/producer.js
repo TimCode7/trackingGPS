138f7c65f4be36a9b9ca8ea3d9e2e2f793a89293
poups6 <pouponthom@cy-tech.fr>
2024-01-17 15:20:23 +0100

    fonction coordonnees aleatoire

--- kafka/producer/producer.js
+++ kafka/producer/producer.js
@@ -1,16 +1,34 @@
 const kafka = require('kafka-node');
-const client = new kafka.KafkaClient({ kafkaHost: 'broker:29092' });
+const client = new kafka.KafkaClient({ kafkaHost: '172.17.7.213:9092' });
 const Producer = kafka.Producer;
 const producer = new Producer(client);
 const topic = 'coordinates';
 
+// fonction pour générer aléatoirement des coordonnées dans un rayon de 1000m autour de la position précédente
+function getRandomCoordinatesAround(latitude, longitude) {
+    const radiusInDegrees = 1000 / 111000;
+    const u = Math.random();
+    const v = Math.random();
+    const w = radiusInDegrees * Math.sqrt(u);
+    const t = 2 * Math.PI * v;
+    const x = w * Math.cos(t);
+    const y = w * Math.sin(t);
+    const new_x = x / Math.cos(latitude);
+    const foundLongitude = parseFloat(new_x) + parseFloat(longitude);
+    const foundLatitude = parseFloat(y) + parseFloat(latitude);
+    return { latitude: foundLatitude, longitude: foundLongitude };
+}
 
 producer.on('ready', function () {
-    setTimeout(() => {
-        const data = {
-            latitude: "-58.096430214146935",
-            longitude: "57.52138240299672"
-        };
+    let i = 0;
+    let data = {
+        latitude: "48.096430214146935",
+        longitude: "0.52138240299672"
+    };
+    setInterval(() => {
+        if (i != 0) {
+            data = getRandomCoordinatesAround(data.latitude, data.longitude);
+        }
         const key = process.env.PRODUCER_KEY;
         const payloads = [
             { topic: topic, messages: JSON.stringify(data), key: key }
@@ -24,7 +42,8 @@ producer.on('ready', function () {
                 console.log("Résultat de l'envoi du message:", data);
             }
         });
-    }, 2000);
+        i++;
+    }, 1000);
 });
 
 producer.on('error', function (err) {
